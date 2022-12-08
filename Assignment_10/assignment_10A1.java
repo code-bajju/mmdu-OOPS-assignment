30d642124182f5c1659d32dce5b5b4bb68a4896e
code-bajju <11212766@mmec.mmdu.org>
2022-12-08 12:34:29 +0530

    Updated Data

--- Assignment_10/assignment_10A1.java
+++ Assignment_10/assignment_10A1.java
@@ -46,25 +46,47 @@ try {
   try (FileWriter f = new FileWriter("bajrang.txt", true);
   BufferedWriter b = new BufferedWriter(f);
   PrintWriter p = new PrintWriter(b);) {
-
+p.println("");
 p.println("appending text into file");
 p.println("I am Inserted Text");
 p.println("I am 2nd Inserted Text");
+System.out.println("Successfully Appended to the file");
 
-} catch (IOException i) {
+} 
+catch (IOException i) {
 i.printStackTrace();
 }
-case '4':
-try{
-    FileWriter fw = new FileWriter(myfile,false);
-    BufferedWriter bw = new BufferedWriter(fw);
-    PrintWriter pw = new PrintWriter(bw);
-    pw.println("Hello My name is code.bajju");
-    pw.close();
-}
-catch(IOException e){
-    System.out.println("An error Occur");
-}
+// case '4':
+// try
+//         {
+//             BufferedReader reader = new BufferedReader(new FileReader(myfile));             
+//             String line = reader.readLine();
+//             while (line != null) 
+//             {
+//                 oldContent = oldContent + line + System.lineSeparator();
+                 
+//                 line = reader.readLine();
+//             }
+//             String newContent = oldContent.replaceAll(oldContent, newLine);
+//             FilterWriter writer = new FileWriter(newContent,true);
+//             writer.write(newLine);
+//         }
+//         catch (Exception e)
+//         {
+//             e.printStackTrace();
+//         }
+//     }
+    break;
+// try{
+//     FileWriter fw = new FileWriter(myfile,false);
+//     BufferedWriter bw = new BufferedWriter(fw);
+//     PrintWriter pw = new PrintWriter(bw);
+//     pw.println("Hello My name is code.bajju");
+//     pw.close();
+// }
+// catch(IOException e){
+//     System.out.println("An error Occur");
+// }
 
 // BufferedReader b = new BufferedReader(new FileReader(myfile));
 // String line = b.readLine();  
@@ -78,7 +100,7 @@ case '5':
 try {  
   if (myfile.delete())  
   {
-    System.out.println(myfile.getName() + " deleted");
+    System.out.println("Succesfully deleted" + myfile.getName());
   } else {
     System.out.println("failed");
   }
@@ -86,11 +108,10 @@ try {
   e.printStackTrace();
 }
 break;
-    default:
-    break;
+    default :
+
+   
     
   }
 }
-
 }
-
