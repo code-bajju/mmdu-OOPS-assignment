3e29db6ffdad7b82aa3cb61788200bf01fa9c722
code-bajju <11212766@mmec.mmdu.org>
2022-12-10 15:11:06 +0530

    Assignment 10 Attempted

--- Assignment_10/assignment_10A1.java
+++ Assignment_10/assignment_10A1.java
@@ -1,118 +1,84 @@
 import java.io.*;
 import java.util.Scanner;
-
 public class assignment_10A1 {
-  public static void main(String[] args) {
-    char operator;
-    String oldContent="Hello Everyone My Name is Bajrang Gour";
-    String newLine = "Hello Everyone My Name is Code.Bajju";
-    String fileContents = "";
-    fileContents = fileContents.replaceAll(oldContent,newLine);
-    File myfile = new File("bajrang.txt");
-  
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter Choice to Perform action on file \n 1.Create a file \n 2.Write Data \n 3.Append Data \n 4.Update Data \n 5.Delete Data ");
-    operator = sc.next().charAt(0);
-    sc.close();
+    public static void main(String[] args) throws IOException {
+        char operator;
+        File myfile = new File("bajrang.txt");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n 1.Create a file \n 2.Write Data \n 3.Append Data \n 4.Update Data \n 5.Delete File ");
+        operator = sc.next().charAt(0);
+        sc.close();
+        switch (operator) {
+            case '1':
+                try {
+                    if (myfile.createNewFile()) {
+                        System.out.println("File " + myfile.getName() + " is created successfully.");
+                    } else {
+                        System.out.println("file is already Exist !");
+                    }
+                } catch (IOException e) {
+                    System.out.println("an Error Occur");
+                    e.printStackTrace();
+                }
+                break;
+            case '2':
+                try {
+                    FileWriter myWriter = new FileWriter("bajrang.txt");
+                    myWriter.write("Hello Everyone My Name is Bajrang Gour");
+                    myWriter.close();
+                    System.out.println("Successfully wrote to the file.");
+                } catch (IOException e) {
+                    System.out.println("An error occurred.");
+                    e.printStackTrace();
+                }
+                break;
+            case '3':
+                try (FileWriter f = new FileWriter("bajrang.txt", true); BufferedWriter b = new BufferedWriter(f); PrintWriter p = new PrintWriter(b);) {
+                    p.println("");
+                    p.println("appending text into file");
+                    p.println("I am Inserted Text");
+                    p.println("I am 2nd Inserted Text");
+                    System.out.println("Successfully Appended to the file");
 
-    switch (operator) {
-    case '1':
-      
-      try {
-        if (myfile.createNewFile()) {
-          System.out.println("File " + myfile.getName() + " is created successfully.");
-        } else {
-          System.out.println("file is already Exist !");
+                } catch (IOException i) {
+                    i.printStackTrace();
+                }
+                break;
+            case '4':
+                try {
+                    String filePath = "bajrang.txt";
+                    Scanner sc2 = new Scanner(new File(filePath));
+                    StringBuffer buffer = new StringBuffer();
+                    while (sc2.hasNextLine()) {
+                        buffer.append(sc2.nextLine() + System.lineSeparator());
+                    }
+                    String fileContents = buffer.toString();
+                    sc2.close();
+                    String oldLine = "Hello";
+                    String newLine = "Namskar";
+                    fileContents = fileContents.replaceAll(oldLine, newLine);
+                    FileWriter writer = new FileWriter(filePath);
+                    System.out.println("");
+                    System.out.println("Successfully Updated to the file");
+                    writer.append(fileContents);
+                    writer.flush();
+                    writer.close();
+                } catch (IOException e) {
+                    System.out.println("error");
+                }
+                break;
+            case '5':
+                try {
+                    if (myfile.delete()) {
+                        System.out.println("Succesfully deleted " + myfile.getName());
+                    } else {
+                        System.out.println("failed");
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                break;
+            default:
         }
-      } catch (IOException e) {
-        System.out.println("an Error Occur");
-        e.printStackTrace();
-      }
-
-      break;
- 
-case '2':
-try {
-    FileWriter myWriter = new FileWriter("bajrang.txt");
-    myWriter.write("Hello Everyone My Name is Bajrang Gour");
-    myWriter.close();
-    System.out.println("Successfully wrote to the file.");
-  } catch (IOException e) {
-    System.out.println("An error occurred.");
-    e.printStackTrace();
-  }
-  break;
-  case '3':
-  try (FileWriter f = new FileWriter("bajrang.txt", true);
-  BufferedWriter b = new BufferedWriter(f);
-  PrintWriter p = new PrintWriter(b);) {
-p.println("");
-p.println("appending text into file");
-p.println("I am Inserted Text");
-p.println("I am 2nd Inserted Text");
-System.out.println("Successfully Appended to the file");
-
-} 
-catch (IOException i) {
-i.printStackTrace();
-}
-
-// case '4':
-// try
-//         {
-//             BufferedReader reader = new BufferedReader(new FileReader(myfile));             
-//             String line = reader.readLine();
-//             while (line != null) 
-//             {
-//                 oldContent = oldContent + line + System.lineSeparator();
-                 
-//                 line = reader.readLine();
-//             }
-//             String newContent = oldContent.replaceAll(oldContent, newLine);
-//             FilterWriter writer = new FileWriter(newContent,true);
-//             writer.write(newLine);
-//         }
-//         catch (Exception e)
-//         {
-//             e.printStackTrace();
-//         }
-//     }
-    break;
-// try{
-//     FileWriter fw = new FileWriter(myfile,false);
-//     BufferedWriter bw = new BufferedWriter(fw);
-//     PrintWriter pw = new PrintWriter(bw);
-//     pw.println("Hello My name is code.bajju");
-//     pw.close();
-// }
-// catch(IOException e){
-//     System.out.println("An error Occur");
-// }
-
-// BufferedReader b = new BufferedReader(new FileReader(myfile));
-// String line = b.readLine();  
-// while (line != null) 
-// {
-//     oldContent = oldContent + line;
-     
-//     line = b.readLine();
-// }
-case '5':
-try {  
-  if (myfile.delete())  
-  {
-    System.out.println("Succesfully deleted" + myfile.getName());
-  } else {
-    System.out.println("failed");
-  }
-} catch (Exception e) {
-  e.printStackTrace();
-}
-break;
-    default :
-
-   
-    
-  }
-}
+    }
 }
